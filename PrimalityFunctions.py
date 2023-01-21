d96cd46e9220b0b070766048550828a3f8ccc5ee
CameronJWalker <cameron.jr.walker@icloud.com>
2023-01-21 13:29:55 -0800

    Finished ListEnd

--- PrimalityFunctions.py
+++ PrimalityFunctions.py
@@ -3,3 +3,16 @@
 #  a number that has no divisors.). You can (and should!) use your 
 #  answer to Exercise 4 to help you. Take this opportunity to 
 #  practice using functions, described below.
+
+num = int(input("Enter a number to see if its prime: "))
+
+# divisors = []
+# x = 0
+# while x < num:
+#     x += 1
+#     y = num % x
+#     if y== 0:
+#         divisors.append(x)
+#     else:
+#         print(str(x) + " is not a factor of " + str(num))
+# print(divisors)
