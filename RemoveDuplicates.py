8d2b26283bce7317123c43bcf9e2da59f4e9fd4b
CameronJWalker <cameron.jr.walker@icloud.com>
2023-01-27 16:14:42 -0800

    finished remove duplicates

---
+++ RemoveDuplicates.py
@@ -0,0 +1,13 @@
+# Write a program (function!) that takes a list and returns a 
+# new list that contains all the elements of the first list 
+# minus all the duplicates.
+
+def removeDup(x):
+    y = []
+    for z in x:
+        if z not in z:
+            y.append(c)
+    return y
+    
+a = [1,2,2,3,4,4,4,5,5,6,7,8,9,10,10,10]
+print(removeDup(a))
